3ad045b625b7df2a3b881747aee597cb63a51722
teoheel <teoheel@gmail.com>
2020-08-15 19:33:40 +0700

    + task3204

--- 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -12,21 +12,20 @@ public class Solution {
     }
 
     public static ByteArrayOutputStream getPassword() {
-
         ByteArrayOutputStream output = new ByteArrayOutputStream();
 
         String letters = "abcdefghijklmnopqrstuvwxyz";
         String digits = "1234567890";
         StringBuilder password = new StringBuilder();
 
-        for (int i=0; i<4; i++) {
-            password.append(letters.charAt((int)(Math.random()*26)));
+        for (int i = 0; i < 4; i++) {
+            password.append(letters.charAt((int) (Math.random() * 26)));
         }
-        for (int i=0; i<2; i++) {
-            password.append(digits.charAt((int)(Math.random()*10)));
+        for (int i = 0; i < 2; i++) {
+            password.append(digits.charAt((int) (Math.random() * 10)));
         }
-        for (int i=0; i<2; i++) {
-            password.append(letters.toUpperCase().charAt((int)(Math.random()*26)));
+        for (int i = 0; i < 2; i++) {
+            password.append(letters.toUpperCase().charAt((int) (Math.random() * 26)));
         }
 
         try {
