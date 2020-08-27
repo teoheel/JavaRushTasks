0092e91f00972d07ad6f48a391708f25683477e4
teoheel <teoheel@gmail.com>
2020-08-27 13:55:20 +0700

    + task3402

--- 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
@@ -13,10 +13,6 @@ public class Solution {
     }
 
     public int factorial(int n) {
-        if (n <= 1) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
-        }
+        return n <= 1 ? 1 : n * factorial(n - 1);
     }
 }
