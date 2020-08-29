84b70a5a8d1e19c1837771ea9248d24243e4900e
teoheel <teoheel@gmail.com>
2020-08-29 16:43:53 +0700

    + task3413

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3413/SoftCache.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task34.task3413;
+
+import java.lang.ref.SoftReference;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SoftCache {
+    private Map<Long, SoftReference<AnyObject>> cacheMap = new ConcurrentHashMap<>();
+
+    public AnyObject get(Long key) {
+        SoftReference<AnyObject> softReference = cacheMap.get(key);
+        return softReference == null ? null : softReference.get();
+    }
+
+    public AnyObject put(Long key, AnyObject value) {
+        SoftReference<AnyObject> softReference = cacheMap.put(key, new SoftReference<>(value));
+        AnyObject o = softReference == null ? null : softReference.get();
+        if (softReference != null) {
+            softReference.clear();
+        }
+        return o;
+    }
+
+    public AnyObject remove(Long key) {
+        SoftReference<AnyObject> softReference = cacheMap.remove(key);
+        AnyObject o = softReference == null ? null : softReference.get();
+        if (softReference != null) {
+            softReference.clear();
+        }
+        return o;
+    }
+}
