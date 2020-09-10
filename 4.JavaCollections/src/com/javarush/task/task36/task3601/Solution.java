3ea0275e58479f15b5bc1c41ec4b5da32ac76165
teoheel <teoheel@gmail.com>
2020-09-10 16:23:34 +0700

    + task3601

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3601/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task36.task3601;
+
+/*
+MVC - простая версия
+*/
+public class Solution {
+    private static final View VIEW = new View();
+
+    public static void main(String[] args) {
+        VIEW.fireShowDataEvent();
+    }
+}
