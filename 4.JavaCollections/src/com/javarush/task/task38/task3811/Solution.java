7b03e916e17e78bd1652566baac79d8cb5ca5749
teoheel <teoheel@gmail.com>
2020-09-22 22:36:04 +0700

    + task3811

--- 4.JavaCollections/src/com/javarush/task/task38/task3811/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3811/Solution.java
@@ -11,6 +11,6 @@ package com.javarush.task.task38.task3811;
 )
 public class Solution {
     public static void main(String[] args) {
-
+        System.out.println(Solution.class.getAnnotation(Ticket.class).toString());
     }
 }
