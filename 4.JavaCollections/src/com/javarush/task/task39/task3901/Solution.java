7ff8c6bd84bb25f7036466d6e7792d10f9b38945
teoheel <teoheel@gmail.com>
2020-09-23 21:18:28 +0700

    + task3901

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3901/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task39.task3901;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Уникальные подстроки
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter your string: ");
+        String s = bufferedReader.readLine();
+
+        System.out.println("The longest unique substring length is: \n" + lengthOfLongestUniqueSubstring(s));
+    }
+
+    public static int lengthOfLongestUniqueSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> visited = new HashMap<>();
+        String output = "";
+        for (int start = 0, end = 0; end < s.length(); end++) {
+            char currChar = s.charAt(end);
+            if (visited.containsKey(currChar)) {
+                start = Math.max(visited.get(currChar) + 1, start);
+            }
+            if (output.length() < end - start + 1) {
+                output = s.substring(start, end + 1);
+            }
+            visited.put(currChar, end);
+        }
+        return output.length();
+    }
+}
